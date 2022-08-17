f27e72e8da87e990dc80d01d4bde68c5eb6e96a2
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-08-17 15:42:29 +0800

    Update GTsprite.py

--- GTsprite.py
+++ GTsprite.py
@@ -3,10 +3,9 @@ from bs4 import BeautifulSoup
 import json
 
 try:
-    soup = BeautifulSoup(requests.get("https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={}".format(NameItem)).text, "html.parser")
-    soup2 =  json.loads(str(soup))
-    soup = BeautifulSoup(requests.get("https://growtopia.fandom.com/wiki/{}".format(soup2["items"][0]["title"])).text, "html.parser")
-    images = soup.find('div', {"class": "card-header"}).img['src']
-    print(images)
+    ItemFinder = BeautifulSoup(requests.get("https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={}".format(NameItem)).text, "html.parser")
+    Item =  json.loads(str(ItemFinder))
+    sprite = BeautifulSoup(requests.get("https://growtopia.fandom.com/wiki/{}".format(Item["items"][0]["title"])).text, "html.parser")
+    images = sprite.find('div', {"class": "card-header"}).img['src'] #result
 except:
     print("Sorry! I can't",NameItem,'in Growtopia Fandom')
