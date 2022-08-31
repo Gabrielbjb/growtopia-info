106eb19498e22b31d3068a183284c5d75d2e8a98
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-08-31 16:06:04 +0800

    Update GTrecipe(en).py

--- GTrecipe(en).py
+++ GTrecipe(en).py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 ItemFinder = requests.get(f"https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={NameItem}").json()
 ItemPage = requests.get("https://growtopia.fandom.com/wiki/{}".format(ItemFinder["items"][0]["title"]))
 HTMLResult = BeautifulSoup(ItemPage.text, "html.parser")
-Properties = {}
+Recipe = {}
 ws = 0
 wd = len(HTMLResult.select(".content"))
 for item in HTMLResult.select(".content"):
@@ -13,13 +13,16 @@ for item in HTMLResult.select(".content"):
     if item.select(".content") == []:
         if wd == ws:
             break
-        Properties[((item.find('th')).text).strip()] = (str(((BeautifulSoup(((str(item.select('td')).replace("</td>", "space")).replace("</li>", "space")).replace("<span>", "space"), "html.parser").get_text(' ', strip=True)).replace("space", "")).replace("\n", "")).strip())
+        Recipe[((item.find('th')).text).strip()] = (str(((BeautifulSoup(((str(item.select('td')).replace("</td>", "space")).replace("</li>", "space")).replace("<span>", "space"), "html.parser").get_text(' ', strip=True)).replace("space", "")).replace("\n", "")).strip())
         ws +=1
     else:
         meh = ((item.find('th')).text).strip()
-        Properties[meh] = {}
+        Recipe[meh] = {}
         if item.select(".content"):
             wd -= 1
             for mes in item.select(".content"):
-                Properties[meh][((mes.find('th')).text).strip()] = (str(((BeautifulSoup(((str(mes.select('td')).replace("</td>", "space")).replace("</li>", "space")).replace("</span>", "space"), "html.parser").get_text(' ', strip=True)).replace("space", "")).replace("\n", "")).strip())
+                Recipe[meh][((mes.find('th')).text).strip()] = (str(((BeautifulSoup(((str(mes.select('td')).replace("</td>", "space")).replace("</li>", "space")).replace("</span>", "space"), "html.parser").get_text(' ', strip=True)).replace("space", "")).replace("\n", "")).strip())
                 ws +=1
+
+# All data will be saved to (Recipe)
+# Growtopia Wiki (id) is still not 100% supported
