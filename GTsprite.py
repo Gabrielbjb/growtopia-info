c611d2e02bde1e016a57e6ae58861a8f5753bb1e
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-08-31 16:11:59 +0800

    Update GTsprite.py

--- GTsprite.py
+++ GTsprite.py
@@ -2,10 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 try:
-    ItemFinder = requests.get(f"https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={NameItem}").json()
-    sprite = BeautifulSoup(requests.get("https://growtopia.fandom.com/wiki/{}".format(ItemFinder["items"][0]["title"])).text, "html.parser")
+    try:
+        ItemFinder = requests.get(f"https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={NameItem}").json()
+        sprite = BeautifulSoup(requests.get("https://growtopia.fandom.com/wiki/{}".format(ItemFinder["items"][0]["title"])).text, "html.parser")
+    except:
+        print("It looks like we can't reach fandom.com")
     images = sprite.find('div', {"class": "card-header"}).img['src'] #result
 except:
-    print("Sorry! I can't find",NameItem,'in Growtopia Fandom')
+    print("Sorry! I can't find",NameItem,'in Growtopia Fandom")
 
 #put item name in (NameItem)
