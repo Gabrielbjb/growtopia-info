4a74f97310be8a08c5cfdb5faf6fbb91e6f9230e
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-09-01 16:07:33 +0800

    Add files via upload

---
+++ GTStatus.py
@@ -0,0 +1,18 @@
+import requests
+from datetime import datetime
+from pytz import timezone
+
+Data = {
+    "Online_User": "",
+    "WOTDLink" : "",
+    "WOTDName" : "",
+    "GTTime" : "",
+    "GTDate" : ""
+}
+
+Website = requests.get(f"https://www.growtopiagame.com/detail").json()
+Data["Online_User"] = Website["online_user"]
+Data["WOTDLink"] = Website["world_day_images"]["full_size"] 
+Data["WOTDName"] = ((Data["WOTDLink"].replace('https://www.growtopiagame.com/worlds/','')).replace('.png','')).upper()
+Data["GTTime"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%X")
+Data["GTDate"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%x")
