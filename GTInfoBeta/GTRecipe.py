f4d3149e961f8836adb8e19e158dbcfa2a03dd9d
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-09-08 00:00:00 +0800

    Update GTRecipe.py

--- GTInfoBeta/GTRecipe.py
+++ GTInfoBeta/GTRecipe.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 try:
+    ItemFinder = requests.get(f"https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={NameItem}").json()
+    ItemPage = requests.get("https://growtopia.fandom.com/wiki/{}".format(ItemFinder["items"][0]["title"]))
     try:
-        ItemFinder = requests.get(f"https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={NameItem}").json()
-        ItemPage = requests.get("https://growtopia.fandom.com/wiki/{}".format(ItemFinder["items"][0]["title"]))
         HTMLResult = BeautifulSoup(ItemPage.text, "html.parser")
         require = []
         Item = {}
@@ -14,14 +14,32 @@ try:
             Recipe = {}
             for item in HTMLResult.select(".content"):
                 if item.select(".content") == []:
-                    if not count in require: 
-                        Recipe[((item.find('th')).text).strip()] = (str(((BeautifulSoup(((str(item.select('td')).replace("</td>","space")).replace("</li>","space")).replace("<span>","space"), "html.parser").get_text(' ', strip=True)).replace("space","")).replace("\n","")).strip()).replace(u'\xa0', u' ')
+                    if not count in require:
+                        if ((item.find('th')).text).strip() in Recipe.keys():
+                            Recipe[((item.find('th')).text).strip()].append([])
+                            for meh in item.select('td'):
+                                meh = ((meh.get_text(' ',strip=True)).replace('"',"'"))
+                                Recipe[((item.find('th')).text).strip()][1].append(meh.replace(u'\xa0', u''))                 
+                        else:
+                            Recipe[((item.find('th')).text).strip()] = [[]]
+                            for meh in item.select('td'):
+                                meh = ((meh.get_text(' ',strip=True)).replace('"',"'"))
+                                Recipe[((item.find('th')).text).strip()][0].append(meh.replace(u'\xa0', u'')) 
                     ws += 1
                 else:
                     meh = ((item.find('th')).text).strip()
                     Recipe[meh] = {}
                     for mes in item.select(".content"):
-                        Recipe[meh][((mes.find('th')).text).strip()] = (str(((BeautifulSoup(((str(mes.select('td')).replace("</td>", "space")).replace("</li>","space")).replace("</span>","space"), "html.parser").get_text(' ', strip=True)).replace("space", "")).replace("\n","")).strip()).replace(u'\xa0', u' ')
+                        if ((mes.find('th')).text).strip() in Recipe[meh].keys():
+                            Recipe[meh][((mes.find('th')).text).strip()].append([])
+                            for the in mes.select('td'):
+                                the = ((the.get_text(' ',strip=True)).replace('"',"'"))
+                                Recipe[meh][((mes.find('th')).text).strip()][1].append(the.replace(u'\xa0', u''))                    
+                        else:
+                            Recipe[meh][((mes.find('th')).text).strip()] = [[]]
+                            for the in mes.select('td'):
+                                the = ((the.get_text(' ',strip=True)).replace('"',"'"))
+                                Recipe[meh][((mes.find('th')).text).strip()][0].append(the.replace(u'\xa0', u'')) 
                         ws += 1
                         require.append(ws)
                 count +=1
@@ -39,13 +57,31 @@ try:
                     for item in HTMLResult.select(".content"):
                         if item.select(".content") == []:
                             if not count in require: 
-                                Recipe[((item.find('th')).text).strip()] = (str(((BeautifulSoup(((str(item.select('td')).replace("</td>","space")).replace("</li>","space")).replace("<span>","space"), "html.parser").get_text(' ', strip=True)).replace("space","")).replace("\n","")).strip()).replace(u'\xa0', u' ')
+                                if ((item.find('th')).text).strip() in Recipe.keys():
+                                    Recipe[((item.find('th')).text).strip()].append([])
+                                    for meh in item.select('td'):
+                                        meh = ((meh.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[((item.find('th')).text).strip()][1].append(meh.replace(u'\xa0', u''))                   
+                                else:
+                                    Recipe[((item.find('th')).text).strip()] = [[]]
+                                    for meh in item.select('td'):
+                                        meh = ((meh.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[((item.find('th')).text).strip()][0].append(meh.replace(u'\xa0', u'')) 
                             ws += 1
                         else:
                             meh = ((item.find('th')).text).strip()
                             Recipe[meh] = {}
                             for mes in item.select(".content"):
-                                Recipe[meh][((mes.find('th')).text).strip()] = (str(((BeautifulSoup(((str(mes.select('td')).replace("</td>","space")).replace("</li>","space")).replace("</span>","space"), "html.parser").get_text(' ', strip=True)).replace("space","")).replace("\n","")).strip()).replace(u'\xa0', u' ')
+                                if ((mes.find('th')).text).strip() in Recipe[meh].keys():
+                                    Recipe[meh][((mes.find('th')).text).strip()].append([])
+                                    for the in mes.select('td'):
+                                        the = ((the.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[meh][((mes.find('th')).text).strip()][1].append(the.replace(u'\xa0', u''))                  
+                                else:
+                                    Recipe[meh][((mes.find('th')).text).strip()] = [[]]
+                                    for the in mes.select('td'):
+                                        the = ((the.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[meh][((mes.find('th')).text).strip()][0].append(the.replace(u'\xa0', u'')) 
                                 ws += 1
                                 require.append(ws)
                         count +=1
@@ -58,14 +94,32 @@ try:
                     for item in itemish.select(".content"):
                         if item.select(".content") == []:
                             if not count in require: 
-                                Recipe[((item.find('th')).text).strip()] = (str(((BeautifulSoup(((str(item.select('td')).replace("</td>","space")).replace("</li>","space")).replace("<span>","space"), "html.parser").get_text(' ', strip=True)).replace("space","")).replace("\n","")).strip()).replace(u'\xa0', u' ')
+                                if ((item.find('th')).text).strip() in Recipe.keys():
+                                    Recipe[((item.find('th')).text).strip()].append([])
+                                    for meh in item.select('td'):
+                                        meh = ((meh.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[((item.find('th')).text).strip()][1].append(meh.replace(u'\xa0', u''))             
+                                else:
+                                    Recipe[((item.find('th')).text).strip()] = [[]]
+                                    for meh in item.select('td'):
+                                        meh = ((meh.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[((item.find('th')).text).strip()][0].append(meh.replace(u'\xa0', u'')) 
                             ws += 1
                         else:
                             meh = ((item.find('th')).text).strip()
                             Recipe[meh] = {}
                             for mes in item.select(".content"):
-                                Recipe[meh][((mes.find('th')).text).strip()] = (str(((BeautifulSoup(((str(mes.select('td')).replace("</td>","space")).replace("</li>","space")).replace("</span>","space"), "html.parser").get_text(' ', strip=True)).replace("space","")).replace("\n","")).strip()).replace(u'\xa0', u' ')
-                                ws += 1
+                                if ((mes.find('th')).text).strip() in Recipe[meh].keys():
+                                    Recipe[meh][((mes.find('th')).text).strip()].append([])
+                                    for the in mes.select('td'):
+                                        the = ((the.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[meh][((mes.find('th')).text).strip()][1].append(the.replace(u'\xa0', u''))                    
+                                else:
+                                    Recipe[meh][((mes.find('th')).text).strip()] = [[]]
+                                    for the in mes.select('td'):
+                                        the = ((the.get_text(' ',strip=True)).replace('"',"'"))
+                                        Recipe[meh][((mes.find('th')).text).strip()][0].append(the.replace(u'\xa0', u'')) 
+                                    ws += 1
                                 require.append(ws)
                         count +=1
                     try:
@@ -74,6 +128,7 @@ try:
                         ItemTitle = (itemish.find('span', class_= "mw-headline").get_text(strip=True)).replace(u'\xa0', u' ')
                     Item[ItemTitle] = Recipe
     except:
-        print("It looks like we can't reach fandom.com")
+        print("Sorry! I can't find",NameItem,"in Growtopia Fandom")
 except:
-    print("Sorry! I can't find",NameItem,"in Growtopia Fandom")
+    print("It looks like we can't reach fandom.com")
+
