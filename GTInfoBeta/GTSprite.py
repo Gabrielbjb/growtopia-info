4a0b50c9d14df51260274b3a5b874cc3b62ea9cd
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-09-13 03:13:34 +0800

    Update GTSprite.py

--- GTInfoBeta/GTSprite.py
+++ GTInfoBeta/GTSprite.py
@@ -1,38 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 
-try:
-    ItemFinder = requests.get(f"https://growtopia.fandom.com/api/v1/SearchSuggestions/List?query={NameItem}").json()
-    HTMLResult = BeautifulSoup(requests.get("https://growtopia.fandom.com/wiki/{}".format(ItemFinder["items"][0]["title"])).text, "html.parser")
+def ItemSprite(NameItem, Region = "en"):
     try:
-        Data = {}  
-        if HTMLResult.select(".wds-tab__content") == []:
-            Data = {
-                "Item" :"",
-                "Tree" :"",
-                "Seed" :""
-            }  
-            images = HTMLResult.find('div', {"class": "gtw-card"})
-            Data["Item"]= (images.find('div', {"class": "card-header"})).img['src']
-            Data["Tree"] = (((((((images.find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).img['src']
-            Data["Seed"] = (images.find('td', {"class": "seedColor"})).img['src']
-        else:
-            for HTMLResultTabber in HTMLResult.select(".wds-tab__content"):
-                Data2 = {
-                    "Item" :"",
-                    "Tree" :"",
-                    "Seed" :""
-                }  
-                images = HTMLResultTabber.find('div', {"class": "gtw-card"})
-                Data2["Item"]= (images.find('div', {"class": "card-header"})).img['src']
-                Data2["Tree"] = (((((((images.find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).img['src']
-                Data2["Seed"] = (images.find('td', {"class": "seedColor"})).img['src']
+        ItemFinder = requests.get(f"https://growtopia.fandom.com/"+Region+"/api/v1/SearchSuggestions/List?query="+NameItem).json()
+        try:
+            ItemPage = requests.get("https://growtopia.fandom.com/"+Region+"/"+"wiki/"+ItemFinder["items"][0]["title"])
+            try:
+                HTMLResult = BeautifulSoup(ItemPage.text, "html.parser")
+                Result = {}  
+                if len(HTMLResult.select(".gtw-card")) == 1:
+                    Data = {
+                        "Item" :"",
+                        "Tree" :"",
+                        "Seed" :""
+                    }  
+                    images = HTMLResult.find('div', {"class": "gtw-card"})
+                    Data["Item"]= (images.find('div', {"class": "card-header"})).img['src']
+                    Data["Tree"] = (((((((images.find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).img['src']
+                    Data["Seed"] = (images.find('td', {"class": "seedColor"})).img['src']
+                    try:
+                        ItemTitle = ((((HTMLResult.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
+                    except:    
+                        ItemTitle = (HTMLResult.find('span', class_= "mw-headline").get_text(strip=True)).replace(u'\xa0', u' ')
+                    Result[ItemTitle] = Data
+                    Result[ItemTitle]["Title"] = ItemTitle
+                else:
+                    for HTMLResultTabber in HTMLResult.select(".wds-tab__content"):
+                        Data = {
+                            "Item" :"",
+                            "Tree" :"",
+                            "Seed" :""
+                        }  
+                        images = HTMLResultTabber.find('div', {"class": "gtw-card"})
+                        Data["Item"]= (images.find('div', {"class": "card-header"})).img['src']
+                        Data["Tree"] = (((((((images.find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).img['src']
+                        Data["Seed"] = (images.find('td', {"class": "seedColor"})).img['src']
+                        try:
+                            ItemTitle = ((((HTMLResultTabber.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
+                        except:    
+                            ItemTitle = (HTMLResultTabber.find('span', class_= "mw-headline").get_text(strip=True)).replace(u'\xa0', u' ')
+                        Result[ItemTitle] = Data
+                        Result[ItemTitle]["Title"] = ItemTitle
                 try:
-                    ItemTitle = ((((HTMLResultTabber.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
-                except:    
-                    ItemTitle = (HTMLResultTabber.find('span', class_= "mw-headline").get_text(strip=True)).replace(u'\xa0', u' ')
-                Data[ItemTitle] = Data2
+                    return Result[ItemFinder["items"][0]["title"]]
+                except:
+                    if NameItem in Result.keys():
+                        return Result[NameItem]
+                    else:
+                        return Result
+            except:
+                return({"Error": "Sorry! I can't find "+NameItem+" in Growtopia Fandom "+Region+" Error Code 3"})            
+        except IndexError:
+            return({"Error": "Sorry! I can't find "+NameItem+" in Growtopia Fandom "+Region+" Error Code 2"})
     except:
-        print("Sorry! I can't find",NameItem,"in Growtopia Fandom")
-except:
-    print("It looks like we can't reach fandom.com") 
+        return({"Error": "It looks like we can't reach fandom.com "+Region+"! Try again later. Error Code 1"})
