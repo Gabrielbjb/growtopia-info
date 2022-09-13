e71e8e15182b6b4b969a0afcd01be4d33b57fda5
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2022-09-13 21:19:24 +0800

    Update GTStatus.py

--- GTStatus.py
+++ GTStatus.py
@@ -1,21 +1,24 @@
-import requests
-from datetime import datetime
-from pytz import timezone
+def GameData():
+    from datetime import datetime
+    try:
+        from pytz import timezone
+    except:
+        return({"Error":"pytz not detected! please install pytz! Error code 1"})
+    Result = {
+        "Online_User": "",
+        "WOTDLink" : "",
+        "WOTDName" : "",
+        "GTTime" : "",
+        "GTDate" : ""
+    }
+    try:
+        Website = requests.get(f"https://www.growtopiagame.com/detail").json()
 
-Data = {
-    "Online_User": "",
-    "WOTDLink" : "",
-    "WOTDName" : "",
-    "GTTime" : "",
-    "GTDate" : ""
-}
-
-Website = requests.get(f"https://www.growtopiagame.com/detail").json()
-Data["Online_User"] = Website["online_user"]
-Data["WOTDLink"] = Website["world_day_images"]["full_size"] 
-Data["WOTDName"] = ((Data["WOTDLink"].replace('https://www.growtopiagame.com/worlds/','')).replace('.png','')).upper()
-Data["GTTime"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%X")
-Data["GTDate"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%x")
-
-# All data will be saved to (Data)
-# You have to install pytz first to use this code, "pip install pytz"
+        Result["Online_User"] = Website["online_user"]
+        Result["WOTDLink"] = Website["world_day_images"]["full_size"] 
+        Result["WOTDName"] = ((Data["WOTDLink"].replace('https://www.growtopiagame.com/worlds/','')).replace('.png','')).upper()
+        Result["GTTime"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%X")
+        Result["GTDate"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%x")
+        return Result
+    except: 
+        return({"Error":"It looks like we can't reach growtopiagame.com! Error code 2"})
