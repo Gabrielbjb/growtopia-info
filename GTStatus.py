9daef20eeea5c30b461143b348e1eddae83324cc
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2023-10-19 12:20:00 +0700

    Update GTStatus.py

--- GTStatus.py
+++ GTStatus.py
@@ -1,12 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
-
+from datetime import datetime
+from pytz import timezone
 def GameData():
-    from datetime import datetime
-    try:
-        from pytz import timezone
-    except:
-        return({"Error":"pytz not detected! please install pytz! Error code 1"})
     Result = {
         "Online_User": "",
         "WOTDLink" : "",
@@ -19,9 +14,10 @@ def GameData():
 
         Result["Online_User"] = Website["online_user"]
         Result["WOTDLink"] = Website["world_day_images"]["full_size"] 
-        Result["WOTDName"] = ((Data["WOTDLink"].replace('https://www.growtopiagame.com/worlds/','')).replace('.png','')).upper()
+        Result["WOTDName"] = ((Result["WOTDLink"].replace('https://www.growtopiagame.com/worlds/','')).replace('.png','')).upper()
         Result["GTTime"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%X")
         Result["GTDate"] = datetime.now(timezone('UTC')).astimezone(timezone('America/New_York')).strftime("%x")
         return Result
     except: 
         return({"Error":"It looks like we can't reach growtopiagame.com! Error code 2"})
+print(GameData())
