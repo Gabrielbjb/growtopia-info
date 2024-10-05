cb9732e089d2163b7e8daa70d68864866779c7d7
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2024-10-06 00:47:55 +0700

    chore: improve redudant code

--- GTItemData.py
+++ GTItemData.py
@@ -1,6 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 
+def fun(HTMLResult, Result):
+    PropertiesResult = []
+    Properties = HTMLResult.find_all('div',  class_= "card-text")
+    Data = HTMLResult.select(".card-field")
+    Rarity = BeautifulSoup((str((HTMLResult.find('small'))).replace("(Rarity: ", "")).replace(")", ""), "html.parser").text
+    for add in Properties:
+        hum = BeautifulSoup(str(add).replace("<br/>", "--split--"), "html.parser")
+        PropertiesResult.append(hum.text)
+    properties = (PropertiesResult[1].strip()).split("--split--")
+    Result.update({"Description": PropertiesResult[0].strip()})
+    Result.update({"Properties": "None" if properties == ['None'] else properties})
+    try:
+        Result.update({"Rarity": int(Rarity)})
+    except:
+        Result.update({"Rarity": "None"})
+    DataResult = []
+    for typ in Data:
+        mus = BeautifulSoup((str(typ).replace("</tr>", ",")).replace("</th>", ","), "html.parser")
+        DataResult = (((mus.text).split(",")))
+    res = 0 
+    while res <= (len(DataResult)-3):
+        Result.update({DataResult[res].strip().replace(" ", ""): DataResult[res+1].strip()})
+        res = res+2
+    check = 0
+    for fix in Result.keys():
+        if check == 3:
+            Result[fix] = Result[fix].split(" - ")
+        if check == 8:
+            Result[fix] = Result[fix].split(" ")
+        if check == 7:
+            restore = []
+            for number in Result[fix].split(" "):
+                input = ""
+                for number2 in number:
+                    if number2.isdigit():
+                        input = input+number2
+                if input != "":
+                    restore.append(input)
+            Result[fix] = {
+                    "Fist":restore[0],
+                    "Pickaxe":restore[1],
+                    "Restore":restore[2]
+            }
+        check +=1
+
 def ItemData(NameItem, Region = "en"):
     try:
         ItemFinder = requests.get(f"https://growtopia.fandom.com/"+Region+"/api/v1/SearchSuggestions/List?query="+NameItem).json()
@@ -10,50 +55,8 @@ def ItemData(NameItem, Region = "en"):
                 Result = {}
                 HTMLResult = BeautifulSoup(ItemPage.text, "html.parser")
                 if len(HTMLResult.select(".gtw-card")) == 1:
-                    Properties = HTMLResult.find_all('div',  class_= "card-text")
-                    Data = HTMLResult.select(".card-field")
-                    Rarity = BeautifulSoup((str((HTMLResult.find('small'))).replace("(Rarity: ", "")).replace(")", ""), "html.parser").text
-                    PropertiesResult = []
-                    for add in Properties:
-                        hum = BeautifulSoup(str(add).replace("<br/>", "--split--"), "html.parser")
-                        PropertiesResult.append(hum.text)
-                    properties = (PropertiesResult[1].strip()).split("--split--")
-                    Result.update({"Description": PropertiesResult[0].strip()})
-                    Result.update({"Properties": "None" if properties == ['None'] else properties})
+                    fun(HTMLResult, Result)
 
-                    try:
-                        Result.update({"Rarity": int(Rarity)})
-                    except:
-                        Result.update({"Rarity": "None"})
-                    DataResult = []
-                    for typ in Data:
-                        mus = BeautifulSoup((str(typ).replace("</tr>", ",")).replace("</th>", ","), "html.parser")
-                        DataResult = (((mus.text).split(",")))
-                    res = 0 
-                    while res <= (len(DataResult)-3):
-                        Result.update({DataResult[res].strip().replace(" ", ""): DataResult[res+1].strip()})
-                        res = res+2
-                    check = 0
-                    for fix in Result.keys():
-                        if check == 3:
-                            Result[fix] = Result[fix].split(" - ")
-                        if check == 8:
-                            Result[fix] = Result[fix].split(" ")
-                        if check == 7:
-                            restore = []
-                            for number in Result[fix].split(" "):
-                                input = ""
-                                for number2 in number:
-                                    if number2.isdigit():
-                                        input = input+number2
-                                if input != "":
-                                    restore.append(input)
-                            Result[fix] = {
-                                    "Fist":restore[0],
-                                    "Pickaxe":restore[1],
-                                    "Restore":restore[2]
-                            }
-                        check +=1
                     try:
                         ItemTitle = ((((HTMLResult.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
                     except:    
@@ -62,52 +65,7 @@ def ItemData(NameItem, Region = "en"):
                 else:
                     for HTMLResultTabber in HTMLResult.select(".gtw-card"):   
                         Result2 = {}   
-                        PropertiesResult = []
-                        Properties = HTMLResultTabber.find_all('div',  class_= "card-text")
-                        Data = HTMLResultTabber.select(".card-field")
-                        Rarity = BeautifulSoup((str((HTMLResultTabber.find('small'))).replace("(Rarity: ","")).replace(")", ""), "html.parser").text
-                        for add in Properties:
-                            hum = BeautifulSoup(str(add).replace("<br/>", "--split--"), "html.parser")
-                            PropertiesResult.append(hum.text)
-                        properties = (PropertiesResult[1].strip()).split("--split--")
-                        Result2.update({"Description": PropertiesResult[0].strip()})
-                        Result2.update({"Properties": "None" if properties == ['None'] else properties})
-
-                        try:
-                            Result2.update({"Rarity": int(Rarity)})
-                        except:
-                            Result2.update({"Rarity": "None"})
-                        DataResult = []
-                        for typ in Data:
-                            mus = BeautifulSoup((str(typ).replace("</tr>", ",")).replace("</th>", ","), "html.parser")
-                            DataResult = (mus.get_text(" ",strip=True)).split(",")
-
-                        res = 0 
-                        while res <= (len(DataResult)-3):
-                            Result2.update({DataResult[res].strip().replace(" ", ""): DataResult[res+1].strip()})
-                            res = res+2  
-
-                        check = 0
-                        for fix in Result2.keys():
-                            if check == 3:
-                                Result2[fix] = Result2[fix].split(" - ")
-                            if check == 8:
-                                Result2[fix] = Result2[fix].split(" ")
-                            if check == 7:
-                                restore = []
-                                for number in Result2[fix].split(" "):
-                                    input = ""
-                                    for number2 in number:
-                                        if number2.isdigit():
-                                            input = input+number2
-                                    if input != "":
-                                        restore.append(input)
-                                Result2[fix] = {
-                                        "Fist":restore[0],
-                                        "Pickaxe":restore[1],
-                                        "Restore":restore[2]
-                                }
-                            check +=1
+                        fun(HTMLResultTabber, Result2)
                         try:
                             ItemTitle = ((((HTMLResultTabber.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
                         except:    
@@ -127,3 +85,8 @@ def ItemData(NameItem, Region = "en"):
             return({"Error": "Sorry! I can't find "+NameItem+" in Growtopia Fandom "+Region+" Error Code 2"})
     except:
         return({"Error": "It looks like we can't reach fandom.com "+Region+"! Try again later. Error Code 1"})
+
+# Example usage
+if __name__ == "__main__":
+    item = ItemData("ancestral tesseract")
+    print(item)
