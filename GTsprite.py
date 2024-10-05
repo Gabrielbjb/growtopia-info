81640b9e41a9f6b26a37b44e593d6cc7ac29868f
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2024-10-06 03:42:16 +0700

    chore: change variable name

--- GTsprite.py
+++ GTsprite.py
@@ -1,57 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
 
-def ItemSprite(NameItem, Region = "en"):
+def get_item_sprite(item_name, region="en"):
     try:
-        ItemFinder = requests.get(f"https://growtopia.fandom.com/"+Region+"/api/v1/SearchSuggestions/List?query="+NameItem).json()
+        search_response = requests.get(f"https://growtopia.fandom.com/{region}/api/v1/SearchSuggestions/List?query={item_name}").json()
         try:
-            ItemPage = requests.get("https://growtopia.fandom.com/"+Region+"/"+"wiki/"+ItemFinder["items"][0]["title"])
+            item_page_response = requests.get(f"https://growtopia.fandom.com/{region}/wiki/{search_response['items'][0]['title']}")
             try:
-                HTMLResult = BeautifulSoup(ItemPage.text, "html.parser")
-                Result = {}  
-                if len(HTMLResult.select(".gtw-card")) == 1:
-                    Data = {
-                        "Item" :"",
-                        "Tree" :"",
-                        "Seed" :""
-                    }  
-                    images = HTMLResult.find('div', {"class": "gtw-card"})
-                    Data["Item"]= (images.find('div', {"class": "card-header"})).img['src']
-                    Data["Tree"] = (((((((images.find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).img['src']
-                    Data["Seed"] = (images.find('td', {"class": "seedColor"})).img['src']
+                html_content = BeautifulSoup(item_page_response.text, "html.parser")
+                result = {}
+                if len(html_content.select(".gtw-card")) == 1:
+                    sprite = {
+                        "Item": "",
+                        "Tree": "",
+                        "Seed": ""
+                    }
+                    images = html_content.find('div', {"class": "gtw-card"})
+                    sprite["Item"] = images.find('div', {"class": "card-header"}).img['src']
+                    sprite["Tree"] = images.find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').img['src']
+                    sprite["Seed"] = images.find('td', {"class": "seedColor"}).img['src']
                     try:
-                        ItemTitle = ((((HTMLResult.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
-                    except:    
-                        ItemTitle = (HTMLResult.find('span', class_= "mw-headline").get_text(strip=True)).replace(u'\xa0', u' ')
-                    Result[ItemTitle] = Data
-                    Result[ItemTitle]["Title"] = ItemTitle
+                        item_title = html_content.find('span', class_="mw-headline").small.decompose().get_text(strip=True).replace(u'\xa0', u' ')
+                    except:
+                        item_title = html_content.find('span', class_="mw-headline").get_text(strip=True).replace(u'\xa0', u' ')
+                    result[item_title] = sprite
+                    result[item_title]["Title"] = item_title
                 else:
-                    for HTMLResultTabber in HTMLResult.select(".wds-tab__content"):
-                        Data = {
-                            "Item" :"",
-                            "Tree" :"",
-                            "Seed" :""
-                        }  
-                        images = HTMLResultTabber.find('div', {"class": "gtw-card"})
-                        Data["Item"]= (images.find('div', {"class": "card-header"})).img['src']
-                        Data["Tree"] = (((((((images.find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).find_next('td')).img['src']
-                        Data["Seed"] = (images.find('td', {"class": "seedColor"})).img['src']
+                    for tab_content in html_content.select(".wds-tab__content"):
+                        sprite = {
+                            "Item": "",
+                            "Tree": "",
+                            "Seed": ""
+                        }
+                        images = tab_content.find('div', {"class": "gtw-card"})
+                        sprite["Item"] = images.find('div', {"class": "card-header"}).img['src']
+                        sprite["Tree"] = images.find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').find_next('td').img['src']
+                        sprite["Seed"] = images.find('td', {"class": "seedColor"}).img['src']
                         try:
-                            ItemTitle = ((((HTMLResultTabber.find('span', class_= "mw-headline")).small).decompose()).get_text(strip=True)).replace(u'\xa0', u' ')
-                        except:    
-                            ItemTitle = (HTMLResultTabber.find('span', class_= "mw-headline").get_text(strip=True)).replace(u'\xa0', u' ')
-                        Result[ItemTitle] = Data
-                        Result[ItemTitle]["Title"] = ItemTitle
+                            item_title = tab_content.find('span', class_="mw-headline").small.decompose().get_text(strip=True).replace(u'\xa0', u' ')
+                        except:
+                            item_title = tab_content.find('span', class_="mw-headline").get_text(strip=True).replace(u'\xa0', u' ')
+                        result[item_title] = sprite
+                        result[item_title]["Title"] = item_title
                 try:
-                    return Result[ItemFinder["items"][0]["title"]]
+                    return result[search_response["items"][0]["title"]]
                 except:
-                    if NameItem in Result.keys():
-                        return Result[NameItem]
+                    if item_name in result.keys():
+                        return result[item_name]
                     else:
-                        return Result
+                        return result
             except:
-                return({"Error": "Sorry! I can't find "+NameItem+" in Growtopia Fandom "+Region+" Error Code 3"})            
+                return {"Error": f"Sorry! I can't find {item_name} in Growtopia Fandom {region} Error Code 3"}
         except IndexError:
-            return({"Error": "Sorry! I can't find "+NameItem+" in Growtopia Fandom "+Region+" Error Code 2"})
+            return {"Error": f"Sorry! I can't find {item_name} in Growtopia Fandom {region} Error Code 2"}
     except:
-        return({"Error": "It looks like we can't reach fandom.com "+Region+"! Try again later. Error Code 1"})
+        return {"Error": f"It looks like we can't reach fandom.com {region}! Try again later. Error Code 1"}
+
+# Example usage
+if __name__ == "__main__":
+    print(get_item_sprite("ancestral tesseract"))
